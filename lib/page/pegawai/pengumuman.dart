4e0ce1b4d60c61d81d2252a6b70552ac4e967404
Irfanka I.S.A <50979146+154-5WH@users.noreply.github.com>
2022-07-12 08:52:07 +0700

    last update function

--- lib/page/pegawai/pengumuman.dart
+++ lib/page/pegawai/pengumuman.dart
@@ -30,7 +30,7 @@ class _PengumumanState extends State<Pengumuman> {
                 children: snapshot.data!.docs.map((DocumentSnapshot document) {
               Map<String, dynamic> data = document.data() as Map<String, dynamic>;
               print(data);
-              return ItemPengumuman(data['Pengumuman'], (data['dibuatPada'] as Timestamp).toDate());
+              return ItemPengumuman(data['pengumuman'], (data['dibuatPada'] as Timestamp).toDate());
             }).toList());
           }),
     );
